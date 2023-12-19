652df15b2a2e041139c59c27692f22b856fc261a
Dayalkar2002 <dayalkarmanish123@gmail.com>
2023-12-19 12:13:38 +0530

    update

--- checking alphabets.py
+++ checking alphabets.py
@@ -1,6 +1,6 @@
 import string
-user_input=input("enter the Value")
-x=set(user_input.lower())
+user_input=input("enter the Value").lower()
+x=set(user_input)
 x.remove(" ")
 print(user_input==set(string.ascii_lowercase))
 print(string.ascii_lowercase)
